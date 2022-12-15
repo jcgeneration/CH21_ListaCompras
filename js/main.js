21f7e989a8dec945599b45cfe0b40ea48393baa2
jc <juan.valencia@idr.edu.mx>
2022-12-15 17:16:02 -0600

    Se agregó la funcionalidad con localStorage para almacenar la lista de forma local

--- js/main.js
+++ js/main.js
@@ -23,8 +23,9 @@ let costoTotal=0;
 let precio=0;
 let cantidad =0;
 let idTimeout;
+let datos = []; // new Array();
 let btnAgregar = document.getElementById("btnAgregar");
-
+let btnClear = document.getElementById("btnClear");
 //Genera un precio al azar
      function getPrecio(){
           return Math.floor(Math.random() * 50 *100) / 100;
@@ -81,14 +82,90 @@ btnAgregar.addEventListener ("click", function(event){
     costoTotal += precio * cantidad;
     precioTotal.innerHTML ="$ "+ costoTotal.toFixed(2);
 
+    localStorage.setItem("contadorProductos", contador);
+    localStorage.setItem("totalEnProductos", totalEnProductos);
+    localStorage.setItem("costoTotal", costoTotal);
+
     let row = `<tr>
-          <td> ${contador} </td>
+          <th> ${contador} </th>
           <td> ${txtNombre.value} </td>
           <td> ${txtNumber.value} </td>
-          <td> ${precio}</td>
+          <td>$ ${precio}</td>
     </tr>`;
     cuerpoTabla[0].insertAdjacentHTML("beforeend", row);
+
+    let elemento = `{
+          "id" : ${contador},
+          "nombre": "${txtNombre.value}",
+          "cantidad" : ${txtNumber.value},
+          "precio": ${precio}
+    }`;
+    
+    datos.push( JSON.parse(elemento) );
+    //console.log(datos);
+    localStorage.setItem("datos", JSON.stringify(datos) );
+
      txtNombre.value="";    
      txtNumber.value="";
     txtNombre.focus();
+}); // click btnAgregar
+
+txtNombre.addEventListener("blur", function (event) {
+     event.preventDefault();
+     // txtNombre.value = txtNombre.value.trim();
+     event.target.value = event.target.value.trim();
+});
+txtNumber.addEventListener("blur", function (event) {
+     event.preventDefault();
+     event.target.value = event.target.value.trim();
+});
+
+window.addEventListener("load", function(event){
+    let tmp= localStorage.getItem("contadorProductos");
+     if(tmp!=null) {
+          contador= parseInt(tmp);
+          contadorProductos.innerHTML= contador;
+     }//if
+     tmp= localStorage.getItem("totalEnProductos");
+     if(tmp!=null) {
+          totalEnProductos= parseInt(tmp);
+          productosTotal.innerHTML=totalEnProductos;
+     }//if
+     tmp= localStorage.getItem("costoTotal");
+     if(tmp!=null) {
+          costoTotal= parseFloat(tmp);
+          precioTotal.innerHTML ="$ "+ costoTotal.toFixed(2);
+     }//if
+
+     tmp= localStorage.getItem("datos");
+     if(tmp!=null){
+          datos = JSON.parse(tmp);
+          datos.forEach(element => {
+               cuerpoTabla[0].innerHTML +=  `<tr>
+               <th>${element.id}</th>
+               <td>${element.nombre}</td>
+               <td>${element.cantidad}</td>
+               <td>$ ${element.precio}</td>
+               </tr> `;
+           });
+
+     }//if
+
+} );
+btnClear.addEventListener ("click", function(event){
+     contador= 0;
+     contadorProductos.innerHTML= contador;
+     totalEnProductos=0;
+     productosTotal.innerHTML=totalEnProductos;
+     costoTotal= 0;
+     precioTotal.innerHTML ="$ "+ costoTotal.toFixed(2);
+     cuerpoTabla[0].innerHTML="";
+
+     localStorage.removeItem("contadorProductos");
+     localStorage.removeItem("totalEnProductos");
+     localStorage.removeItem("costoTotal");
+     localStorage.removeItem("datos");
+     
+     localStorage.clear();
+
 });
